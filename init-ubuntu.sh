c7253c33b7dd8874f844b8610d2ef63d06aeeeaa
Håkon Klausen <hakon.klausen@ae.no>
2021-01-13 14:57:36 +0100

    Added install of pyton libraries

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -23,3 +23,8 @@ echo "Installing Miniconda..."
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
+#
+# Installing python libraries
+#
+wget "${downloadUrlBase}/python-libraries.txt"
+pip install -r requirements.txt
