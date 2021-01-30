0ad2965c22d7f28bb6384b799d020cf0605f1ce6
Håkon Klausen <hakon.klausen@ae.no>
2021-01-30 09:27:43 +0100

    Added libeccodes-tools to init-ubuntu install Separated R install into separete file

---
+++ install-r.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+sudo apt install r-base
+sudo apt install build-essential
